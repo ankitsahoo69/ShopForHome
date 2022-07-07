efed7a66cbacb6375b207992981e9950eb2d4377
ankitsahoo69 <ankitsahoo69@gmail.com>
2022-07-07 07:13:49 +0530

    controller added

--- Backend/routes/products.js
+++ Backend/routes/products.js
@@ -1,118 +1,25 @@
-const Product = require('../models/product');
 const express = require('express');
-const multer = require('multer');
-const mongoose = require("mongoose");
 const router = express.Router();
+const extractFile = require('../middleware/file');
+const ProductController = require('../controllers/products');
 
 
-const MIME_TYPE_MAP = { 'image/png': 'png', 'image/jpeg': 'jpg', 'image/jpg': 'jpg' };
+router.get('/', ProductController.getProducts);
 
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        const isValid = MIME_TYPE_MAP[file.mimetype];
-        let error = new Error('Invalid mime type');
-        if (isValid) {
-            error = null;
-        }
-        cb(error, "images");
-    },
-    filename: (req, file, cb) => {
-        const name = file.originalname.toLowerCase().split(" ").join("-");
-        const ext = MIME_TYPE_MAP[file.mimetype];
-        cb(null, name + "-" + Date.now() + "." + ext);
-    }
-})
+router.get('/:id', ProductController.getProduct);
 
 
-const upload = multer({ storage: storage });
+router.post('/', extractFile, ProductController.createProduct);
 
 
-router.get('/', async (req, res) => {
-    const product = await Product.find();
-    if (!product) return res.status(500).send('cannot get product');
-    res.status(200).send(product);
-})
+router.put('/:id', extractFile, ProductController.updateProduct);
 
 
-router.get('/:id', async (req, res) => {
-    if (!mongoose.isValidObjectId(req.params.id)) {
-        return res.status(400).send('invalid product id');
-    }
-    const product = await Product.findById(req.params.id);
-    if (!product) return res.status(500).send('cannot get product');
-    res.status(200).send(product);
-})
+router.delete('/:id', ProductController.deleteProduct);
 
 
-router.post('/', upload.single('image'), (req, res) => {
-    if (!req.file) return res.status(400).send('image not found');
-    const url = req.protocol + "://" + req.get("host");
-    imagePath = url + "/images/" + req.file.filename;
-    const product = new Product({
-        name: req.body.name,
-        description: req.body.description,
-        image: imagePath,
-        price: req.body.price,
-        category: req.body.category,
-        stock: req.body.stock
-    })
-    product.save().then(() => {
-        res.status(200).send(product);
-    }).catch(() => {
-        res.status(500).send('cannot add product');
-    })
-})
-
-
-router.put('/:id', upload.single('image'), async (req, res) => {
-    if (!mongoose.isValidObjectId(req.params.id)) {
-        return res.status(400).send('invalid product id');
-    }
-    const product = await Product.findById(req.params.id);
-    if (!product) return res.status(400).send('invalid product');
-    let imagePath;
-    if (req.file) {
-        const url = req.protocol + "://" + req.get("host");
-        imagePath = url + "/images/" + req.file.filename;
-    }
-    else {
-        imagepath = product.image;
-    }
-    const updatedProduct = await Product.findByIdAndUpdate(
-        req.params.id,
-        {
-            name: req.body.name,
-            description: req.body.description,
-            image: imagePath,
-            price: req.body.price,
-            category: req.body.category,
-            stock: req.body.stock
-        },
-        { new: true }
-    )
-    if (!updatedProduct) return res.status(500).send('cannot update product');
-    res.status(200).send(updatedProduct);
-})
-
-
-router.delete('/:id', async (req, res) => {
-    if (!mongoose.isValidObjectId(req.params.id)) {
-        return res.status(400).send('invalid product id');
-    }
-    const product = await Product.findByIdAndRemove(req.params.id);
-    if (!product) res.status(500).send('cannot delete product');
-    res.status(200).send(product);
-})
-
-
-router.get('/get/count', async (req, res) => {
-    const productCount = await Product.countDocuments();
-    if (!productCount) {
-        return res.status(500).json({ success: false });
-    }
-    res.send({ productCount: productCount });
-})
+router.get('/get/count', ProductController.getProductCount);
 
 
 module.exports = router;
--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -1,126 +1,29 @@
-const User = require('../models/user');
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const mongoose = require("mongoose");
+const UserController = require('../controllers/users');
+
+router.get('/', UserController.getUsers);
 
 
-router.get('/', async (req, res) => {
-    const user = await User.find();
-    if (!user) return res.status(500).send('cannot get user');
-    res.status(200).send(user);
-})
+router.get('/:id', UserController.getUser);
 
 
-router.get('/:id', async (req, res) => {
-    if (!mongoose.isValidObjectId(req.params.id)) {
-        return res.status(400).send('invalid user id');
-    }
-    const user = await User.findById(req.params.id);
-    if (!user) return res.status(500).send('cannot get user');
-    res.status(200).send(user);
-})
+router.post('/', UserController.createUser);
 
 
-router.post('/', async (req, res) => {
-    let user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        passwordHash: bcrypt.hashSync(req.body.password, 10),
-        phone: req.body.phone,
-        admin: req.body.admin,
-        address: req.body.address,
-        city: req.body.city,
-        country: req.body.country
-    })
-    user = await user.save();
-    if (!user) return res.status(500).send('cannot create user');
-    res.send(user);
-})
+router.post('/register', UserController.registerUser);
 
 
-router.post('/register', async (req, res) => {
-    let user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        passwordHash: bcrypt.hashSync(req.body.password, 10),
-        phone: req.body.phone,
-        admin: req.body.admin,
-        address: req.body.address,
-        city: req.body.city,
-        country: req.body.country
-    })
-    const existUser = await User.findOne({ email: req.body.email });
-    if (existUser) {
-        return res.status(500).send('email already exists');
-    } else {
-        user = await user.save();
-        if (!user) return res.status(400).send('cannot create user');
-        res.send(user);
-    }
-})
+router.post('/login', UserController.loginUser);
 
 
-router.post('/login', async (req, res) => {
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) res.status(500).send('cannot find user');
-    if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
-        const token = jwt.sign({ userId: user.id, admin: user.admin }, 'secret', { expiresIn: '1d' })
-        res.status(200).send({ name: user.name, id: user.id, token: token })
-    } else {
-        res.status(400).send('wrong password');
-    }
-})
+router.put('/:id', UserController.updateUser);
 
 
-router.put('/:id', async (req, res) => {
-    if (!mongoose.isValidObjectId(req.params.id)) {
-        return res.status(400).send('invalid user id');
-    }
-    const oldUser = await User.findById(req.params.id);
-    let newPassword;
-    if (req.body.password) {
-        newPassword = bcrypt.hashSync(req.body.password, 10);
-    } else {
-        newPassword = oldUser.passwordHash;
-    }
-    const user = await User.findByIdAndUpdate(
-        req.params.id,
-        {
-            name: req.body.name,
-            email: req.body.email,
-            passwordHash: bcrypt.hashSync(req.body.password, 10),
-            phone: req.body.phone,
-            admin: req.body.admin,
-            address: req.body.address,
-            city: req.body.city,
-            country: req.body.country
-        },
-        { new: true }
-    )
-    if (!user) return res.status(500).send('cannot update user');
-    res.status(200).send(user);
-})
+router.delete('/:id', UserController.deleteUser);
 
 
-router.delete('/:id', async (req, res) => {
-    if (!mongoose.isValidObjectId(req.params.id)) {
-        return res.status(400).send('invalid user id');
-    }
-    const user = await User.findByIdAndRemove(req.params.id);
-    if (!user) res.status(500).send('cannot delete user');
-    res.status(200).send(user);
-})
-
-
-router.get('/get/count', async (req, res) => {
-    const userCount = await User.countDocuments();
-    if (!userCount) {
-        res.status(500).json({ success: false });
-    }
-    res.send({ userCount: userCount });
-})
+router.get('/get/count', UserController.getUserCount);
 
 
 module.exports = router;
